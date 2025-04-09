640712488fe14ac1385bd61c88e14be19ae7211c
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-04-10 02:00:16 +0530

    Fixed cors issue

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
-app.use(cors())
+app.use(cors({ origin: process.env.FRONTEND_URL }))
 app.use(express.json())
 app.use(cookiesParser())
 
