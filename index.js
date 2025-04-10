5cb24542b404bfc2d5f753a110d68a5fca7c27e1
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-04-10 09:21:44 +0530

    Minor Changes

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookiesParser())
 
 const PORT = process.env.PORT || 8080
+
+connectDB().then(()=>{
+    server.listen(PORT,()=>{
+        console.log("server running at " + PORT)
+    })
+})
 
 app.get('/',(request,response)=>{
     response.json({
@@ -20,9 +30,3 @@ app.get('/',(request,response)=>{
 
 //api endpoints
 app.use('/api',router)
-
-connectDB().then(()=>{
-    server.listen(PORT,()=>{
-        console.log("server running at " + PORT)
-    })
-})
